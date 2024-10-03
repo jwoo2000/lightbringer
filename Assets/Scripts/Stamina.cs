b77d0a99a03bad9645dc3f06b1bb8b2da365cf8c
KateGriffiths <rggri@student.unimelb.edu.au>
2024-10-03 17:46:07 +1000

    Features: Added a basic attack using pellet prefab, created simple UI to display health and stamina

--- Assets/Scripts/Stamina.cs
+++ Assets/Scripts/Stamina.cs
@@ -9,8 +9,13 @@ public class Stamina : MonoBehaviour
     public float regenerationRate;
     private float stamina;
     public Transform height;
+    private float health;
+    private float maxHealth;
 
-    public Transform StaminaBar;
+    public Image StaminaBar;
+    public Image HealthBar;
+
+    //public Transform StaminaBar;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +27,8 @@ public class Stamina : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        health = GameManager.gameManager._playerHealth.Health;
+        maxHealth = GameManager.gameManager._playerHealth.MaxHealth;
         if (Input.GetKey(KeyCode.LeftShift) && stamina > 0) {
             stamina -= Time.deltaTime;
         } else if ((Input.GetKey(KeyCode.LeftShift) && stamina <= 0)) {
@@ -35,7 +42,10 @@ public class Stamina : MonoBehaviour
             stamina = 0;
         }
         height.position = new Vector3(0.0f, stamina, 0.0f);
-        StaminaBar.localScale = new Vector3(0.2f, stamina / maxStamina, 0.2f);
-        StaminaBar.position = new Vector3(StaminaBar.position.x, 1 + (stamina / maxStamina) / 2, StaminaBar.position.z);
+        StaminaBar.fillAmount = stamina / maxStamina;
+        HealthBar.fillAmount = health / maxHealth;
+
+        /*StaminaBar.localScale = new Vector3(0.2f, stamina / maxStamina, 0.2f);
+        StaminaBar.position = new Vector3(StaminaBar.position.x, 1 + (stamina / maxStamina) / 2, StaminaBar.position.z); */
     }
 }
