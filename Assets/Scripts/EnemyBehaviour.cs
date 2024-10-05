ab15c004bbacef0f53b32cd367089c139dc0bd28
KateGriffiths <rggri@student.unimelb.edu.au>
2024-10-05 18:11:52 +1000

    Created Small enemy unity, fixed hit boxes on enemies

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,34 +4,53 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    private Transform target; 
+    public enum EnemyType{
+        Small,
+        Medium,
+        Large
+    }
+
     public float speed = 1.0f;
-    private float distanceToTarget;
+    public float damageAmount = 20.0f;
+    public float startingHealth = 100.0f;
+    public EnemyType Behaviourtype;
+    public UnitHealth _enemyHealth; 
+
+    //Medium class specific
     public float AttackWaitTime = 0.5f;
-    private float AttackWindUp;
     public float LungeSpeed = 7.0f;
+    public float AttackTriggerRange = 3.0f;
+
+    private Transform target; 
+    private float distanceToTarget;
+    private float AttackWindUp;
     private Vector3 AttackTarget;
     private Rigidbody rb;
     private float AttackCoolDown; 
     private bool DamageCoolDown;
-    public float AttackTriggerRange = 3.0f;
-
-    public float damageAmount = 20.0f;
-
     private string tagToDamage = "Player";
 
-    public UnitHealth _enemyHealth = new UnitHealth(100.0f, 100.0f);
 
 
     // Start is called before the first frame update
     void Start()
     {
+        _enemyHealth = new UnitHealth(startingHealth, startingHealth);
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         AttackWindUp = 0.0f;
         AttackCoolDown = 0.0f;
         DamageCoolDown = false;
         rb = GetComponent<Rigidbody>();
     }
+
+    /*Different Enemy Behaviours
+    
+    small - deals damage on contact with player, slow speed, freezes temporarily after dealing damage, follows player
+    medium - deals damage on contact while lunging, medium speed, follows player
+    large - slow speed, keeps distance from player, continuously spawns medium and small enemies, has slow swing attack if player gets close
+
+    */
+
 
     // Update is called once per frame
     void Update()
@@ -42,24 +61,57 @@ public class EnemyBehaviour : MonoBehaviour
         }
         distanceToTarget = Vector3.Distance(target.position, transform.position);
 
-        if (AttackCoolDown > 0) {
-            AttackCoolDown -= Time.deltaTime;
-            AttackTarget = target.position;
-        } else if (distanceToTarget < AttackTriggerRange || AttackWindUp > 0) {
-            rb.velocity = -1.0f * speed * transform.forward;
-            AttackWindUp += Time.deltaTime;
+        
 
-            
-            if (AttackWindUp > AttackWaitTime) {
-                Attack();
-                
+        switch(Behaviourtype)
+        {
+            case EnemyType.Small:
+            {
+                if (AttackCoolDown <= 0) 
+                {
+                    DamageCoolDown = false;
+                    AttackTarget = target.position;
+                    rb.velocity = speed * transform.forward;
+                } 
+                else 
+                {
+                    AttackCoolDown -= Time.deltaTime;
+                    
+                }
+
+                break;
             }
-        } else {
-            AttackTarget = target.position;
-            rb.velocity = speed * transform.forward;
-            //transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            case EnemyType.Medium:
+            {
+                if (AttackCoolDown > 0) 
+                {
+                    AttackCoolDown -= Time.deltaTime;
+                    AttackTarget = target.position;
+                } 
+                else if (distanceToTarget < AttackTriggerRange || AttackWindUp > 0) 
+                {
+                rb.velocity = -1.0f * speed * transform.forward;
+                AttackWindUp += Time.deltaTime;
+                } 
+                else 
+                {
+                    AttackTarget = target.position;
+                    rb.velocity = speed * transform.forward;
+                }
+                    
+                if (AttackWindUp > AttackWaitTime) {
+                    Attack();
+                }
+                break;
+            }
+            case EnemyType.Large:
+            {
+                break;
+            }
+            default: break;
         }
-        transform.LookAt(target);
+        transform.LookAt(target, Vector3.up);
+        transform.eulerAngles = new Vector3(0,transform.eulerAngles.y,0);
 
     }
 
@@ -74,7 +126,6 @@ public class EnemyBehaviour : MonoBehaviour
         AttackWindUp = 0.0f;
         AttackCoolDown = 0.5f;
         DamageCoolDown = false;
-        //  Replace this with collision detection with player
     }
 
     private void OnTriggerEnter(Collider col)
