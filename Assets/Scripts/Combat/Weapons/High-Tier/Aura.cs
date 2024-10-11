c62cc52f72f9200db0b7da2615893be856b5c02f
jwoo2000 <jeongwoo@student.unimelb.edu.au>
2024-10-11 17:39:41 +1100

    add aura implementation

--- Assets/Scripts/Combat/Weapons/High-Tier/Aura.cs
+++ Assets/Scripts/Combat/Weapons/High-Tier/Aura.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 
 public class Aura : AreaWeapon
 {
+    [SerializeField]
+    private float aoeSize;
+    [SerializeField]
+    private float baseAoe;
+
+    [SerializeField]
+    private float baseDmgCD;
+
+    [SerializeField]
+    private AuraArea activeAuraArea;
+    [SerializeField]
+    private GameObject activeAuraAreaInstance;
+
     // init values for Aura
     private void Awake()
     {
         weaponTier = Weapon.Tier.High;
         weaponName = "Blazing Radiance";
-        speedLabel = "Damage frequency";
+        speedLabel = "Damage Speed";
         speedDesc = "Increases aura damage frequency";
         uniqueLabel = "AOE Size";
         uniqueDesc = "Increases aura size";
@@ -22,24 +35,30 @@ public class Aura : AreaWeapon
         // aura has a long duration around player
         areaDuration = 999.0f;
         damageCD = 1.0f;
+        baseDmgCD = damageCD;
+        aoeSize = 5.0f;
+        baseAoe = aoeSize;
     }
 
     public override void Fire()
     {
-        GameObject areaInstance = Instantiate(weaponObject, playerTransform.position + weaponOriginOffset, Quaternion.identity, transform);
-        AuraArea auraArea = areaInstance.GetComponent<AuraArea>();
-        auraArea.damage = getDamage();
-        auraArea.lifetime = areaDuration;
-        auraArea.damageCD = damageCD;
+        activeAuraAreaInstance = Instantiate(weaponObject, playerTransform.position + weaponOriginOffset, Quaternion.identity, transform);
+        activeAuraAreaInstance.transform.localScale = Vector3.one * aoeSize;
+        activeAuraArea = activeAuraAreaInstance.GetComponent<AuraArea>();
+        activeAuraArea.damage = getDamage();
+        activeAuraArea.lifetime = areaDuration;
+        activeAuraArea.damageCD = damageCD;
     }
 
     protected override void upgradeSpeed()
     {
-        
+        damageCD = (baseDmgCD * (1 - (speedLevel * 0.15f)));
+        activeAuraArea.damageCD = damageCD;
     }
 
     protected override void upgradeUnique()
     {
-
+        aoeSize = (baseAoe * (1 + (uniqueLevel * 0.3f)));
+        activeAuraAreaInstance.transform.localScale = Vector3.one * aoeSize;
     }
 }
