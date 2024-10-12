55f80104b9614159996d05623f3810f74b850392
jwoo2000 <jeongwoo@student.unimelb.edu.au>
2024-10-12 11:54:05 +1100

    reworked player spawner to wave system

--- Assets/Scripts/Combat/Enemies/EnemyPlayerSpawner.cs
+++ Assets/Scripts/Combat/Enemies/EnemyPlayerSpawner.cs
@@ -4,77 +4,130 @@ using UnityEngine;
 
 public class EnemyPlayerSpawner : MonoBehaviour
 {
-    [SerializeField] private float baseSpawnRate = 10.0f; 
-    [SerializeField] private GameObject enemySmall; 
+    [SerializeField] private bool isSpawning;
+
+    [SerializeField] private GameObject enemySmall;
     [SerializeField] private GameObject enemyMedium;
-    [SerializeField] private GameObject enemyLarge;  
-    [SerializeField] private bool isSpawning = true; 
-    [SerializeField] private float spawnDistance = 10.0f;
-    GameObject player;
-    private WeaponController weaponController;
-    private PlayerStats playerStats;
+    [SerializeField] private GameObject enemyLarge;
+
+    [SerializeField] private WeaponController weaponController;
+    [SerializeField] private PlayerStats playerStats;
+
+    [SerializeField] private float baseSpawnDistance; // closest distance enemies are spawned (harder enemies spawn further out)
+
+    [SerializeField] private float baseSpawnWaveCD; // slowest wave CD (low levels ~lv0)
+    [SerializeField] private float minSpawnWaveCD; // fastest wave CD (high levels ~lv20)
+    [SerializeField] private float spawnWaveCD; // seconds between waves
+    [SerializeField] private float spawnRateScaling; // per level, decrease the CD by this % of the difference between the current CD and the minimum CD
+
+    [SerializeField]
+    private List<EnemyBehaviour.EnemyType> spawnWave = new List<EnemyBehaviour.EnemyType>();
+
     private int playerLevel;
 
-    private float spawnRate;
-
-
-    void Awake()
+    private void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        weaponController = player.GetComponent<WeaponController>();
-        playerStats = player.GetComponent<PlayerStats>();
+        isSpawning = true;
+        baseSpawnDistance = 15.0f;
+        baseSpawnWaveCD = 30.0f;
+        minSpawnWaveCD = 5.0f;
+        spawnRateScaling = 0.1f;
+        spawnWaveCD = baseSpawnWaveCD;
     }
 
-    void Start() {
-        spawnRate = baseSpawnRate;
+    private void Start()
+    {
         StartCoroutine(Spawner());
-        playerLevel = playerStats.level;
     }
 
-    private IEnumerator Spawner () {
-        Debug.Log("spawn rate:" + baseSpawnRate);
-        while (isSpawning) {
-            WaitForSeconds wait = new WaitForSeconds(spawnRate);
-            yield return wait;
+    private IEnumerator Spawner()
+    {
+        while (isSpawning)
+        {
+            Debug.Log("Spawning wave in: " + spawnWaveCD + " seconds.");
+            yield return new WaitForSeconds(spawnWaveCD);
+            SpawnWave();
+            Debug.Log("Wave: "+spawnWave);
+
             playerLevel = playerStats.level;
-            spawnRate = 20.0f / (playerLevel + 2);
-            Debug.Log(spawnRate);
-            if (weaponController.HighWeapon != null) 
+            spawnWave.Clear();
+
+            // player has low tier wep
+            if (weaponController.LowWeapon != null || weaponController.MidWeapon != null || weaponController.HighWeapon != null)
             {
-                int pick = Random.Range(0, 4);
-                if (pick == 0)
+                for (int i = 0; i < playerLevel; i++)
                 {
-                    Instantiate(enemyLarge, SpawnPosition(spawnDistance * 2.0f), Quaternion.identity);
-                }
-                else
-                {
-                    Instantiate(enemyMedium, SpawnPosition(spawnDistance * 1.5f), Quaternion.identity);
+                    // add small enemies
+                    spawnWave.Add(EnemyBehaviour.EnemyType.Small);
                 }
             }
-            else if (weaponController.MidWeapon != null) 
+            // player has mid tier wep
+            if (weaponController.MidWeapon != null || weaponController.HighWeapon != null)
             {
-                int pick = Random.Range(0, 2);
-                if (pick == 0)
+                for (int i = 0; i < (playerLevel/2); i++)
                 {
-                    Instantiate(enemySmall, SpawnPosition(spawnDistance), Quaternion.identity);
-                }
-                else 
-                {
-                    Instantiate(enemyMedium, SpawnPosition(spawnDistance * 1.5f), Quaternion.identity);
+                    // add medium enemies
+                    spawnWave.Add(EnemyBehaviour.EnemyType.Medium);
                 }
             }
-            else if (weaponController.LowWeapon != null) 
+            // player has high tier wep
+            if (weaponController.HighWeapon != null)
             {
-                Instantiate(enemySmall, SpawnPosition(spawnDistance), Quaternion.identity);
+                for (int i = 0; i < (playerLevel/10)*2; i++)
+                {
+                    // add large enemies
+                    spawnWave.Add(EnemyBehaviour.EnemyType.Large);
+                }
             }
+
+            // update spawn cd for next wave
+            spawnWaveCD = getSpawnCD(playerStats.level);
         }
     }
 
-    private Vector3 SpawnPosition(float distance) {
-        int deg = Random.Range(0, 360);
-        float rad = deg * Mathf.Deg2Rad;
-
-        return (new Vector3(distance * Mathf.Sin(rad), 1, distance * Mathf.Cos(rad)) + transform.position);
+    private float getSpawnCD(int playerLevel)
+    {
+        return (minSpawnWaveCD + ((baseSpawnWaveCD - minSpawnWaveCD) * Mathf.Exp(-spawnRateScaling * (float) playerLevel)));
     }
 
+    private void SpawnWave()
+    {
+        float spawnDist;
+        foreach (EnemyBehaviour.EnemyType mobType in spawnWave)
+        {
+            spawnDist = baseSpawnDistance;
+            switch (mobType)
+            {
+                case EnemyBehaviour.EnemyType.Medium:
+                    spawnDist += 1.0f;
+                    break;
+                case EnemyBehaviour.EnemyType.Large:
+                    spawnDist += 2.0f;
+                    break;
+            }
+            Instantiate(getMobFromType(mobType), SpawnPosition(spawnDist), Quaternion.identity);
+        }
+    }
+
+    private Vector3 SpawnPosition(float distance)
+    {
+        float rad = Random.Range(0, 360) * Mathf.Deg2Rad;
+
+        return (transform.position + new Vector3(Mathf.Sin(rad), 0, Mathf.Cos(rad))*distance);
+    }
+
+    private GameObject getMobFromType(EnemyBehaviour.EnemyType type)
+    {
+        switch (type)
+        {
+            case EnemyBehaviour.EnemyType.Small:
+                return enemySmall;
+            case EnemyBehaviour.EnemyType.Medium:
+                return enemyMedium;
+            case EnemyBehaviour.EnemyType.Large:
+                return enemyLarge;
+            default:
+                return null;
+        }
+    }
 }
